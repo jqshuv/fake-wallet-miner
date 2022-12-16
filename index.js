1dc239fa5c696105a32cfdab60aeeca0b4168ad2
Joshua Schmitt <jqshuv@gmail.com>
2022-12-16 08:20:44 +0100

    fix: added a few fixes

--- index.js
+++ index.js
@@ -6,18 +6,30 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 let isContinuing = true
 
+function setTerminalTitle(title) {
+  process.stdout.write(String.fromCharCode(27) + "]0;" + title + String.fromCharCode(7));
+}
+
 async function main() {
     const bitcoinValue = await geckoAPI.getBitcoinPrice()
+    let xtry = 0
+
+    setTerminalTitle("Starting...")
+    console.log("\nCurrent Bitcoin Value: " + bitcoinValue + "\n")
+    console.log("\nInitializing...\n")
+    await sleep(5000)
 
     while (isContinuing === true) {
-        await sleep(5)
+        await sleep(10)
+        xtry++
+        setTerminalTitle("BTC-MINER (by @jqshuv) Try: " + xtry)
 
-        const randomInt = crypto.randomInt(0,300)
+        const randomInt = crypto.randomInt(0,10000)
 
         if (randomInt <= 1) {
             let randomBitcoinValue = crypto.randomInt(1,100)/100
             console.log(colors.fg.white + "> 0x" + crypto.randomBytes(20).toString('hex') + colors.fg.green + " > " + randomBitcoinValue.toString() + " BTC ($" + Math.round(bitcoinValue * randomBitcoinValue) + ")")
-            let answer = prompt("> Continue? (y/n) ");
+            let answer = prompt("> Continue? (y/n)");
             if (answer.toLowerCase() === "y" || answer.toLowerCase() === "true") {
                 isContinuing = true
             } else if (answer.toLowerCase() === "n" || answer.toLowerCase() === "false") {
